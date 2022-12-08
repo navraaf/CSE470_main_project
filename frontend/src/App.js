5c20814b52725878ce593a662cbb3973c7485e34
navraaf <navraaf.2661@gmail.com>
2022-12-08 16:35:41 +0600

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import Header from './component/Header' 
+import Footer from './component/Footer'  
+const App=() => {
+  return (
+    <>
+      <Header  /> 
+      <main> 
+        <h1>Welcome To Medcrest</h1> 
+      </main> 
+      <Footer />
+     
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const header = () => {
+  return <header>header</header>;
+};
+
+export default header;
+
