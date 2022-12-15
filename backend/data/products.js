c7b2a307c149b5660fb85a9418ae7d76d9723160
navraaf <navraaf.2661@gmail.com>
2022-12-16 05:39:46 +0600

    backend

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    
+    name: 'Napa',
+    image: '/images/napa.jpg',
+    description:
+      'Napa is indicated for fever,common cold and influenza, headache,bodyache and post vaccination pain in children',
+    brand: 'Beximco Pharmaceuticals Ltd',
+    category: 'Tablet Medicine',
+    price: 20,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Napa 60ml Syrup',
+    image: '/images/napasyrup.jpg',
+    description:
+      'Napa 60ml Syrup is a medicine used to relieve pain and to reduce fever. It is used to treat many conditions such as headache, body ache, toothache and common cold.',
+    brand: 'Beximco Pharmaceuticals Ltd',
+    category: 'Liquid Medicine',
+    price: 35,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    
+    name: 'Adovas',
+    image: '/images/adovas.jpg',
+    description:
+      'Adovas is a sugar free, non sedating herbal cough syrup.',
+    brand: 'Square Pharmaceuticals Ltd',
+    category: 'Syrup',
+    price: 70,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Metaspray',
+    image: '/images/metaspray.jpg',
+    description:
+      'Metaspray Nasal spray is a medicine used to prevent and treat allergic symptoms like sneezing and runny nose due to allergies',
+    brand: 'Cipla Ltd',
+    category: 'Nasal Spray',
+    price: 220,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Deslor',
+    image: '/images/deslor.jpg',
+    description:
+      'Deslor 5 MG Tablet is an antihistamine medicine that contains Desloratadine. It is used to relieve allergy symptoms like runny nose, sneezing, redness and swelling of the nose, watery eyes, itching, rash etc.',
+    brand: 'Sun Pharma Laboratories Ltd',
+    category: 'Tablet',
+    price: 5,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    
+    name: 'Sergel 20',
+    image: '/images/sergel.jpg',
+    description:
+      'Sergel 20 is a medicine which reduces the amount of acid produced in your stomach. It is used to treat heartburn, acid reflux and problems in your food pipe.',
+    brand: 'Healthcare Pharmaceuticals Ltd',
+    category: 'Tablet',
+    price: 65,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
